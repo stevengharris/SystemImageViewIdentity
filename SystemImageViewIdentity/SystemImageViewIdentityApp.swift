0612f1224792009e78db30a8f521662646da2f7c
Steven G. Harris <steven.g.harris@gmail.com>
2022-12-02 09:56:14 -0800

    Initial Commit

---
+++ SystemImageViewIdentity/SystemImageViewIdentityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemImageViewIdentityApp.swift
+//  SystemImageViewIdentity
+//
+//  Created by Steven Harris on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SystemImageViewIdentityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
